2e24dc50593d8b73eae77d371028eba34de7c2c8
Andrii Popov <andrew.p@codesushi.co>
2018-05-11 18:41:26 +0000

    initial routes config

---
+++ config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    
+    'default' => [
+        'controller' => 'DefaultController',
+        'action' => 'indexAction'
+    ],
+    
+    'book_list' => [
+        'controller' => 'BookController',
+        'action' => 'indexAction'
+    ],
+    
+    'feedback' => [
+        'controller' => 'FeedbackController',
+        'action' => 'contactAction'
+    ]
+    
+];
