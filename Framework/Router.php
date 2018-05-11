d887762d1e07ab09251528ba31d93b8ed87312f4
Andrii Popov <andrew.p@codesushi.co>
2018-05-11 18:41:59 +0000

    inject routes from config in router

--- Framework/Router.php
+++ Framework/Router.php
@@ -4,6 +4,13 @@ namespace Framework;
 
 class Router
 {
+    private $routes;
+    
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+    
     public static function redirect($to)
     {
         header("Location: {$to}");
